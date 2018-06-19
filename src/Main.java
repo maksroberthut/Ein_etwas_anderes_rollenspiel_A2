e8bb7ae8bd271fa8c31235c3a879aabcb418062f
muelinga <im0602@live.de>
2018-06-19 21:31:30 +0200

    Frame, Label und Buttons

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class Main extends JFrame {
 
     public static void main (String[]args){
-        
+
+        GUI gui = new GUI();
+        gui.initialize();
     }
 }
